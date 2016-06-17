2167d147387a6516ce10727490b4e7043f5975b2
AramayisO <aramorkusyan@gmail.com>
2016-06-17 16:38:30 -0700

    Add num_datapoints function to cdv-reader module
    Function reads csv file and returns the number of lines of data in the
    file.

--- csv-reader.py
+++ csv-reader.py
@@ -3,4 +3,20 @@
 csv-reader
 @author: Aramayis
 """
-     
+
+import csv
+
+def num_datapoints(filename):
+    """ Reads csv file and returns number of lines in file, where each line
+    represents data for a different timestamp.
+    
+    Keyword arguments:
+    filename -- file path for the data e.g. Data/apple_stocks.csv
+    
+    """
+    num = 0
+    with open(filename, newline='') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        for row in csvreader:
+            num += 1
+    return num - 1      
